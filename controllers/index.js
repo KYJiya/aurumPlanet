201bd1d55a7e5d1326e5e63c0c17dba5288a0847
kwonYongjun <kwon_yongjun@khu.ac.kr>
2021-05-19 11:07:22 +0900

    clear oneAPI

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,42 @@
-const Highlight = require('../models/highlight');
+const { Theme, User, Page, Highlight } = require('../models');
 
 async function oneAPI (req, res, next) {
     try {
-        const highlight = await Highlight.create({
+        const pages = await Page.findOrCreate({
+            attributes: ['pageId'],
+            where: {
+                pageUrl: req.body.pageUrl,
+            },
+        });
+
+        const pageId = await pages.map((pageId) => pageId.pageId);
+
+        const themes = await Theme.findAll({
+            attributes: ['themeId', 'color1'],
+            where: {
+                color1: req.body.colorHex,
+            },
+        });
+
+        const theme = await themes.map((theme) => theme.themeId);
+
+        const highlights = await Highlight.create({
+            text: req.body.text,
+            themeId: theme[0],
+            pageId: pageId[0],
+        });
+
+        const highlight = await highlights.dataValues.highlightId;
+
+        await highlights.addUser(req.body.userId);
+        
+        res.status(201).json({
+            highlightId: highlight,
             userId: req.body.userId,
-            pageUrl: req.body.pageUrl,
+            pageId: pageId[0],
             colorHex: req.body.colorHex,
             text: req.body.text,
         });
-        console.log(highlight);
-        res.status(201).json(highlight);
     } catch (err) {
         console.error(err);
         next(err);
